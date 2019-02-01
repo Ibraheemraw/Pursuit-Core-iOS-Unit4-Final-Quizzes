a3d0e644ac5764c9dcc0fd69639b16368a79f71c
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-02-01 11:34:09 -0500

    got my alert to show up on the profile vc

--- Quizzes/Quizzes/Controllers/ProfileVC.swift
+++ Quizzes/Quizzes/Controllers/ProfileVC.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ProfileVC: UIViewController {
     
+    @IBOutlet weak var profileImageObj: UIButton!
+    
+    @IBOutlet weak var usernameObj: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupProfileAlert()
         
     }
     
-   
+
+    
+    @IBAction func setupUserProfileImage(_ sender: UIButton) {
+    }
+  private func setupProfileAlert(){
+    let alertController = UIAlertController.init(title: "Please Create Your Profile📲", message: "Enter in your username and setup your picture. No spaces or special characters are allowed🙅🏾‍♂️", preferredStyle: .alert)
+   let cancel = UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil)
+   let submit = UIAlertAction.init(title: "Sumbit", style: .default, handler: { (ProfileCreated) in
+    
+    })
+    alertController.addAction(cancel)
+    alertController.addAction(submit)
+    self.present(alertController, animated: true, completion: nil)
+    }
 }
