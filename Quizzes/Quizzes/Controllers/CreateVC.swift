fd90792cffd480024e50b019cc83feda46db9b7d
ibraheemraw <ibraheemrawlinson@pursuit.org>
2019-02-01 17:17:41 -0500

    was able to add the bar button items and make the create button only enabled when you filled in text in the textfield

--- Quizzes/Quizzes/Controllers/CreateVC.swift
+++ Quizzes/Quizzes/Controllers/CreateVC.swift
@@ -9,22 +9,52 @@
 import UIKit
 
 class CreateVC: UIViewController {
-
+    @IBOutlet weak var titleTextFeildObj: UITextField!
+    @IBOutlet weak var factOneDescriptionObj: UITextView!
+    @IBOutlet weak var factTwoDescriptionObj: UITextView!
+    @IBOutlet weak var CreateQuizitem: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Create A Quiz📝"
-        // Do any additional setup after loading the view.
+        titleTextFeildObj.delegate = self
+        factOneDescriptionObj.delegate = self
+        factTwoDescriptionObj.delegate = self
+        CreateQuizitem.isEnabled = false
+        //accessToCreateAQuiz()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func accessToCreateAQuiz(){
+        guard  let title = titleTextFeildObj.text else {
+            fatalError("Something wrong when unwrapping values ")
+        }
+        if title == ""  {
+            CreateQuizitem.isEnabled = false
+            
+        } else {
+            CreateQuizitem.isEnabled = true
+        }
     }
-    */
+    private func requiredFillinAlert(){}
+    @IBAction func createQuizBttnPressed(_ sender: UIBarButtonItem) {
+    }
+}
 
+extension CreateVC: UITextFieldDelegate, UITextViewDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.becomeFirstResponder()
+        
+    }
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.becomeFirstResponder()
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        accessToCreateAQuiz()
+        return true
+    }
+    func textViewDidChange(_ textView: UITextView) {
+        
+    }
+    
 }
